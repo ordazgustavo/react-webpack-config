86697dd95c28fead5a0dbaf91b333a1f5b4d5ebc
Gustavo Ordaz <ordazsgustavo@gmail.com>
2018-05-23 20:05:07 -0500

    Initial commit

---
+++ src/containers/Users.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Users extends Component {
+  render () {
+    return (
+      <div>
+        <h1>Awesome users on board of this course</h1>
+      </div>
+    )
+  }
+}
+
+export default Users
